8308321fbdc9648b0045c71977987bf3da250f75
Bill Weakley <bweakley@bottomline.com>
2020-01-17 14:32:00 -0500

    INK-12294 - Logicalink: IPad Size is causing characters to be cut off at the Bottom of the IPad screen and causing to need to scroll
    Signed-off-by: Bill Weakley <bweakley@bottomline.com>

--- Source/CDMarkdownParser.swift
+++ Source/CDMarkdownParser.swift
@@ -88,7 +88,7 @@ open class CDMarkdownParser {
             self.paragraphStyle = paragraphStyle
         } else {
             let paragraphStyle = NSMutableParagraphStyle()
-            paragraphStyle.paragraphSpacing = 0
+            paragraphStyle.paragraphSpacing = 3
             paragraphStyle.paragraphSpacingBefore = 0
             paragraphStyle.lineSpacing = 1.38
             self.paragraphStyle = paragraphStyle
