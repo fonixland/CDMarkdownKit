8cde55dccfeb01c34800f55c9142349425cf883d
Bill Weakley <bweakley@bottomline.com>
2020-01-30 22:39:13 -0500

    INK-12294 - Logicalink: IPad Size is causing characters to be cut off at the Bottom of the IPad screen and causing to need to scroll
    Signed-off-by: Bill Weakley <bweakley@bottomline.com>

--- Source/CDMarkdownParser.swift
+++ Source/CDMarkdownParser.swift
@@ -70,7 +70,7 @@ open class CDMarkdownParser {
     public let paragraphStyle: NSParagraphStyle
 
     // MARK: - Initializer
-    public init(font: CDFont = CDFont.systemFont(ofSize: 30),
+    public init(font: CDFont = CDFont.systemFont(ofSize: 11),
                 boldItalicFont: CDFont? = nil,
                 boldFont: CDFont? = nil,
                 italicFont: CDFont? = nil,
